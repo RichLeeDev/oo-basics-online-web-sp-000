d492e4efb4d92b1571776850e14c20c881aa1d4a
Richard Lee <richlee81192@gmail.com>
2019-09-30 21:05:07 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,7 +9,26 @@ class Book
   
   def author=(author)
     @author = author
+  end
+  
+  def author 
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num 
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre(genre)
+    @genre 
   end 
   
+  def genre=(genre)
+    @genre = genre
+  end 
 end 
 
